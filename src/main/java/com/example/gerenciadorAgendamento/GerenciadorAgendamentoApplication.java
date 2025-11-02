7c57807ef826f978a73138f1c8c901e6f049b7c2
lbarreto-tech <lucaslbcs2003@gmail.com>
2025-11-02 14:55:03 -0300

    Projeto criado

---
+++ src/main/java/com/example/gerenciadorAgendamento/GerenciadorAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciadorAgendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorAgendamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciadorAgendamento/GerenciadorAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciadorAgendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorAgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
